28c72b083668197683bddbf7ef90fa4eae6f5ae2
Mike <mike.micatka@gmail.com>
2026-07-30 21:46:03 +0000

    ruff

--- libs/protein-conformational-ensemble/src/protein_conformational_ensemble/cli/__init__.py
+++ libs/protein-conformational-ensemble/src/protein_conformational_ensemble/cli/__init__.py
@@ -1,4 +1,4 @@
-# libs/protein-conformational-ensemble/src/protein_conformational_ensemble/cli/__init__.py
+# libs/protein-conformational-ensemble/src/protein_conformational_ensemble/cli/__init__.py # noqa: E501
 
 from .generate_manifest import generate_manifest
 
